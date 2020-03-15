e09c5f419f6fd7eaa3e7259244b473e90d01a405
Andrei Bezmen <andryshabez26@gmail.com>
2020-03-15 22:22:08 +0300

    feat: add slider

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+//           MENU SWITCH
+
+
 const navList = document.querySelector('nav');
 navList.addEventListener('click', () => {
   navList.querySelectorAll('.item-href').forEach(item => item.classList.remove('active'));
@@ -6,3 +9,55 @@ navList.addEventListener('click', () => {
 })
 
 
+//           SLIDER
+
+
+let devices = document.querySelectorAll('.devices');
+let currentDevice = 0;
+let isEnabled = true;
+
+function changeCurrentDevice(n) {
+  currentDevice = (n + devices.length) % devices.length
+}
+
+function hideDevice(direction) {
+  isEnabled = false;
+  devices[currentDevice].classList.add(direction);
+  devices[currentDevice].addEventListener('animationend', function() {
+    this.classList.remove('active-item', direction)
+  })
+}
+
+function showDevice(direction) {
+  devices[currentDevice].classList.add('next', direction);
+  devices[currentDevice].addEventListener('animationend', function() {
+    this.classList.remove('next', direction);
+    this.classList.add('active-item');
+    isEnabled = true;
+  })
+}
+
+function previousDevice(n) {
+  hideDevice('to-right')
+  changeCurrentDevice(n - 1)
+  showDevice('from-left')
+}
+
+function nextDevice(n) {
+  hideDevice('to-left')
+  changeCurrentDevice(n + 1)
+  showDevice('from-right')
+}
+
+
+document.querySelector('.control.left').addEventListener('click', function() {
+  if (isEnabled) {
+    previousDevice(currentDevice);
+  }
+})
+
+document.querySelector('.control.right').addEventListener('click', function() {
+  if (isEnabled) {
+    nextDevice(currentDevice);
+  }
+})
